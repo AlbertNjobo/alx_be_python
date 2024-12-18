fe1c3f52a8c55e3773e30829b2ddeba3035db0ca
Lawrence Njobo <n02212525l@students.ac.zw>
2024-12-18 14:37:14 +0200

    Amended basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -7,7 +7,7 @@ multiplication = number1 * number2
 
 print(f"Addition of {number1} and {number2} is {addition}")
 print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}"
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
 
 
 
