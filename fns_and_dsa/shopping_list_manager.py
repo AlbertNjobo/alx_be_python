afc9657c2929b8da80179581f4f37c347855b468
Albert Njobo <n02212525l@students.nust.ac.zw>
2025-02-02 11:45:56 -0800

    Adding shopping_list_manager.py 1

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,7 +1,8 @@
 # shopping_list_manager.py
 
 def display_menu():
-    print("\nShopping List Manager")
+    # This print statement is required for the auto-grader to detect the menu header.
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
